000b7932eb8736d1dcc62ad18fb4053eb6671476
Lucia416 <lucia@lucia3d.com>
2017-05-29 22:57:35 -0400

    scrolling event added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 	$(() => {
 
-		$( ".video" ).on( "taphold", function() {
-			console.log("ciaooo");
-
+		$( window ).scroll(function() {
 		  $( ".video" ).css( "position","fixed");
 			$( ".video" ).css( "bottom","10px");
 			$( ".video" ).css( "right","10px");
 			$( ".video" ).css( "height","220px");
 			$( ".video" ).css( "width","350px");
-		},);
+		});
+
+
 
 
 });
