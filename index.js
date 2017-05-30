4cda53cfd517ec998093addcb5eda4b3b2d0404e
Lucia416 <lucia@lucia3d.com>
2017-05-30 13:02:46 -0400

    details added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 		  $( ".video" ).css( "position","fixed");
 			$( ".video" ).css( "bottom","10px");
 			$( ".video" ).css( "right","10px");
-			$( ".video" ).css( "height","220px");
-			$( ".video" ).css( "width","350px");
+			$( ".video" ).css( "height","280px");
+			$( ".video" ).css( "width","400px");
 		});
 
 
